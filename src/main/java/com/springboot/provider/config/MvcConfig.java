5f845921d2d4580a2a6e65826c4f950b908472a2
XuZhenkui <xuzhenk@bsoft.com.cn>
2020-12-23 11:15:47 +0800

    修改：设置默认的内容格式类型

--- src/main/java/com/springboot/provider/config/MvcConfig.java
+++ src/main/java/com/springboot/provider/config/MvcConfig.java
@@ -2,10 +2,8 @@ package com.springboot.provider.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurationSupport {
@@ -40,4 +38,14 @@ public class MvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/img/**").addResourceLocations("file:"+ header);
     }
 
+    /**
+     * Override this method to configure content negotiation.
+     *
+     * @param configurer
+     * @see DefaultServletHandlerConfigurer
+     */
+    @Override
+    protected void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON, MediaType.TEXT_XML, MediaType.APPLICATION_XML);
+    }
 }
