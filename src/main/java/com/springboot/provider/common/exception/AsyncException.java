3dd9e15e47800ce4d541201d70a51a6df603d6bd
XuZhenkui <xuzhenk@bsoft.com.cn>
2021-04-20 11:51:14 +0800

    新增异步配置

---
+++ src/main/java/com/springboot/provider/common/exception/AsyncException.java
@@ -0,0 +1,26 @@
+package com.springboot.provider.common.exception;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+
+import java.lang.reflect.Method;
+
+public class AsyncException implements AsyncUncaughtExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Handle the given uncaught exception thrown from an asynchronous method.
+     *
+     * @param ex     the exception thrown from the asynchronous method
+     * @param method the asynchronous method
+     * @param params the parameters used to invoked the method
+     */
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        logger.error("异步执行错误, message={}", ex.getMessage());
+        logger.error("发生错误的方法: {}, 参数: {}", method.getName(), JSON.toJSONString(params));
+    }
+}
---
+++ src/main/java/com/springboot/provider/config/AsyncConfig.java
@@ -0,0 +1,41 @@
+package com.springboot.provider.config;
+
+import com.springboot.provider.common.exception.AsyncException;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+    /**
+     * The {@link Executor} instance to be used when processing async
+     * method invocations.
+     */
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() / 2 + 1);
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() + 1);
+        executor.setQueueCapacity(256);
+        executor.setThreadNamePrefix("Application AsyncThread::");
+        executor.initialize();
+        return executor;
+    }
+
+    /**
+     * The {@link AsyncUncaughtExceptionHandler} instance to be used
+     * when an exception is thrown during an asynchronous method execution
+     * with {@code void} return type.
+     */
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncException();
+    }
+
+
+}
