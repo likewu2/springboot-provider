cbf2a090b934d96768888a16e0b3301119150e1b
XuZhenkui <xuzhenk@bsoft.com.cn>
2022-05-26 14:57:07 +0800

    feat: update spring-security config

--- src/main/java/com/springboot/provider/config/WebSecurityConfig.java
+++ src/main/java/com/springboot/provider/config/WebSecurityConfig.java
@@ -1,52 +1,43 @@
 package com.springboot.provider.config;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * @Description
+ * @Project springboot-provider
+ * @Package com.springboot.provider.config
+ * @Author xuzhenkui
+ * @Date 2022-05-26 09:28
+ */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    /**
-     * Override this method to configure {@link WebSecurity}. For example, if you wish to
-     * ignore certain requests.
-     *
-     * Endpoints specified in this method will be ignored by Spring Security, meaning it
-     * will not protect them from CSRF, XSS, Clickjacking, and so on.
-     *
-     * Instead, if you want to protect endpoints against common vulnerabilities, then see
-     * {@link #configure(HttpSecurity)} and the {@link HttpSecurity#authorizeRequests}
-     * configuration method.
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .headers()
                     .frameOptions()
                     // .disable(); // 任何跨域
-                    .sameOrigin(); // 同源跨域
-
-        http
+                    .sameOrigin() // 同源跨域
+                .and()
                     .cors()
                 .and()
                     .csrf().disable()
@@ -60,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated();
 //                .and()
 //                    .formLogin()
-////                    .loginPage("/page/login.html")
+//                    .loginPage("/page/login.html")
 //                    .successForwardUrl("/index.html")
 //                    .permitAll();
 //                .and()
@@ -69,52 +60,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                    .invalidateHttpSession(true)
 //                    .clearAuthentication(true)
 //                    .permitAll();
+
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
-    /**
-     * Override this method to configure {@link WebSecurity}. For example, if you wish to
-     * ignore certain requests.
-     *
-     * Endpoints specified in this method will be ignored by Spring Security, meaning it
-     * will not protect them from CSRF, XSS, Clickjacking, and so on.
-     *
-     * Instead, if you want to protect endpoints against common vulnerabilities, then see
-     * {@link #configure(HttpSecurity)} and the {@link HttpSecurity#authorizeRequests}
-     * configuration method.
-     */
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-    }
-
-    /**
-     * Allows modifying and accessing the {@link UserDetailsService} from
-     * {@link #userDetailsServiceBean()} without interacting with the
-     * {@link ApplicationContext}. Developers should override this method when changing
-     * the instance of {@link #userDetailsServiceBean()}.
-     * @return the {@link UserDetailsService} to use
-     */
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 
-        UserDetails user =
-                User.withUsername("user")
-                        .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("123"))
-                        .roles("ADMIN")
-                        .build();
-
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
         return new InMemoryUserDetailsManager(user);
-    }
-
-    /*
-    * 多个继承关系用 \n 隔开即可，如下 ROLE_A > ROLE_B \n ROLE_C > ROLE_D。
-    * 如果角色层级关系是连续的，也可以这样配置 ROLE_A > ROLE_B > ROLE_C > ROLE_D。
-    * */
-    @Bean
-    RoleHierarchy roleHierarchy(){
-        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
-        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-        return hierarchy;
     }
 }
---
+++ src/main/java/com/springboot/provider/config/WebSecurityConfigDeprecated.java
@@ -0,0 +1,121 @@
+package com.springboot.provider.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @deprecated Use a {@link WebSecurityConfig} Bean to configure
+ */
+@Deprecated
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfigDeprecated extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * Override this method to configure {@link WebSecurity}. For example, if you wish to
+     * ignore certain requests.
+     *
+     * Endpoints specified in this method will be ignored by Spring Security, meaning it
+     * will not protect them from CSRF, XSS, Clickjacking, and so on.
+     *
+     * Instead, if you want to protect endpoints against common vulnerabilities, then see
+     * {@link #configure(HttpSecurity)} and the {@link HttpSecurity#authorizeRequests}
+     * configuration method.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                    .frameOptions()
+                    // .disable(); // 任何跨域
+                    .sameOrigin(); // 同源跨域
+
+        http
+                    .cors()
+                .and()
+                    .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                    .antMatchers("/", "/index.html","/websocket/**", "/api/**", "/test/**", "/his/**", "/lis/**").permitAll()
+                    .antMatchers("/js/**", "/css/**", "/images/**", "/lib/**", "/page/**").permitAll()
+                    .antMatchers("/user/**").hasRole("USER")
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/file/**").hasAnyRole("USER","ADMIN")
+                    .anyRequest().authenticated();
+//                .and()
+//                    .formLogin()
+////                    .loginPage("/page/login.html")
+//                    .successForwardUrl("/index.html")
+//                    .permitAll();
+//                .and()
+//                    .logout()
+//                    .deleteCookies()
+//                    .invalidateHttpSession(true)
+//                    .clearAuthentication(true)
+//                    .permitAll();
+    }
+
+    /**
+     * Override this method to configure {@link WebSecurity}. For example, if you wish to
+     * ignore certain requests.
+     *
+     * Endpoints specified in this method will be ignored by Spring Security, meaning it
+     * will not protect them from CSRF, XSS, Clickjacking, and so on.
+     *
+     * Instead, if you want to protect endpoints against common vulnerabilities, then see
+     * {@link #configure(HttpSecurity)} and the {@link HttpSecurity#authorizeRequests}
+     * configuration method.
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
+
+    /**
+     * Allows modifying and accessing the {@link UserDetailsService} from
+     * {@link #userDetailsServiceBean()} without interacting with the
+     * {@link ApplicationContext}. Developers should override this method when changing
+     * the instance of {@link #userDetailsServiceBean()}.
+     * @return the {@link UserDetailsService} to use
+     */
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user =
+                User.withUsername("user")
+                        .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("123"))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /*
+    * 多个继承关系用 \n 隔开即可，如下 ROLE_A > ROLE_B \n ROLE_C > ROLE_D。
+    * 如果角色层级关系是连续的，也可以这样配置 ROLE_A > ROLE_B > ROLE_C > ROLE_D。
+    * */
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return hierarchy;
+    }
+}
