7c185a26fe4da98dd9691b207baa13e9218a0eeb
XuZhenkui <xuzhenk@bsoft.com.cn>
2022-08-03 13:07:12 +0800

    feat: 修改异步线程池配置

--- src/main/java/com/springboot/provider/config/AsyncConfig.java
+++ src/main/java/com/springboot/provider/config/AsyncConfig.java
@@ -12,6 +12,9 @@ import java.util.concurrent.Executor;
 @EnableAsync
 @Configuration
 public class AsyncConfig extends AsyncConfigurerSupport {
+
+    private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+
     /**
      * The {@link Executor} instance to be used when processing async
      * method invocations.
@@ -19,8 +22,8 @@ public class AsyncConfig extends AsyncConfigurerSupport {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() / 2 + 1);
-        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() + 1);
+        executor.setCorePoolSize(AVAILABLE_PROCESSORS + 1);
+        executor.setMaxPoolSize(AVAILABLE_PROCESSORS * 2);
         executor.setQueueCapacity(256);
         executor.setThreadNamePrefix("Application AsyncThread::");
         executor.initialize();
